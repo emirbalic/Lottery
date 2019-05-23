d6a9ee7a20cec4acc7f3b2c11468154accf592aa
Emir Balic <emirbalic@gmail.com>
2019-05-24 00:04:34 +0200

    base skeleton

---
+++ src/Lottery.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Ball from './Ball';
+import './Lottery.css';
+
+class Lottery extends Component {
+  static defaultProps = {
+    title: 'Lotto',
+    maxBalls: 6,
+    maxNum: 40
+  };
+  constructor(props) {
+    super(props);
+    this.state = { nums: Array.from({ length: this.props.maxBalls }) };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  generate() {
+    this.setState(currentState => ({
+      nums: currentState.nums.map(
+        n => Math.floor(Math.random() * this.props.maxNum) + 1
+      )
+    }));
+  }
+  handleClick() {
+    this.generate();
+  }
+  render() {
+    return (
+      <section className="Lotery">
+        <h1>{this.props.title}</h1>
+        <div>
+          {this.state.nums.map(n => <Ball num={n} />)}
+        </div>
+        <button onClick={this.handleClick}>Generate</button>
+      </section>
+    );
+  }
+}
+export default Lottery;
